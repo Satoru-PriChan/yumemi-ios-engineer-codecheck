dcca6f36d61711ee15a7b7cf3325fc58a0d01c37
Kent Strong <chihayayome@gmail.com>
2025-02-25 21:49:17 +0900

    Feat: Add retain cycle prevention

--- iOSEngineerCodeCheck/DetailViewController.swift
+++ iOSEngineerCodeCheck/DetailViewController.swift
@@ -19,16 +19,16 @@ final class DetailViewController: UIViewController {
     @IBOutlet private var forksLabel: UILabel!
     @IBOutlet private var openIssuesLabel: UILabel!
 
-    var searchViewController: SearchViewController!
+    weak var searchViewController: SearchViewController?
 
     // MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        guard let selectedIndex = searchViewController.selectedIndex,
-              searchViewController.fetchedRepositories.indices.contains(selectedIndex) else { return }
-        let selectedRepository = searchViewController.fetchedRepositories[selectedIndex]
+        guard let selectedIndex = searchViewController?.selectedIndex,
+              searchViewController?.fetchedRepositories.indices.contains(selectedIndex) ?? false,
+            let selectedRepository = searchViewController?.fetchedRepositories[selectedIndex] else { return }
 
         titleLabel.text = selectedRepository["full_name"] as? String ?? "Unknown Repository"
         languageLabel.text = "Written in \(selectedRepository["language"] as? String ?? "Unknown Language")"
@@ -42,9 +42,9 @@ final class DetailViewController: UIViewController {
     // MARK: - Private functions
 
     private func fetchAndSetImage() {
-        guard let selectedIndex = searchViewController.selectedIndex,
-              searchViewController.fetchedRepositories.indices.contains(selectedIndex),
-              let owner = searchViewController.fetchedRepositories[selectedIndex]["owner"] as? [String: Any],
+        guard let selectedIndex = searchViewController?.selectedIndex,
+              searchViewController?.fetchedRepositories.indices.contains(selectedIndex) ?? false,
+              let owner = searchViewController?.fetchedRepositories[selectedIndex]["owner"] as? [String: Any],
               let imgURLString = owner["avatar_url"] as? String,
               let imgURL = URL(string: imgURLString)
         else {
