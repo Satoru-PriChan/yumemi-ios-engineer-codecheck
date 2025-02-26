6db3d52cdf6b00113924905346919fbb80f2ee67
Kent Strong <chihayayome@gmail.com>
2025-02-26 17:07:41 +0900

    Clean: Add XCTContext.runActivity in UITest

--- iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
+++ iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
@@ -19,47 +19,51 @@ class iOSEngineerCodeCheckUITests: XCTestCase, Sendable {
     }
     
     func testSearchAndDetailViewFlow() {
-        // MARK: - 1. SearchViewController で検索を実行
-        app.launch()
-        // 画面が表示されるまで待機
-        let searchField = app.otherElements["Search"]
-        waitToAppear(for: searchField)
-        XCTAssertTrue(searchField.exists, "Search bar should exist")
+        XCTContext.runActivity(named: "SearchViewController 検索実行") { (activity) in
+            app.launch()
+            let searchField = app.otherElements["Search"]
+            waitToAppear(for: searchField)
+            XCTAssertTrue(searchField.exists, "Search bar should exist")
+            searchField.tap()
+            searchField.typeText("Swift\n") // "Swift" を検索
+        }
         
-        searchField.tap()
-        searchField.typeText("Swift\n") // "Swift" を検索
+        XCTContext.runActivity(named: "SearchViewController 検索結果タップ") { (activity) in
+            // 検索結果が表示されるまで待機
+            let firstCell = waitToHittable(for: app.tables.cells.element(boundBy: 0))
+            XCTAssertTrue(firstCell.exists, "Search results should be displayed")
+            firstCell.tap()
+        }
         
-        // 検索結果が表示されるまで待機
-        let firstCell = waitToHittable(for: app.tables.cells.element(boundBy: 0))
-        XCTAssertTrue(firstCell.exists, "Search results should be displayed")
+        XCTContext.runActivity(named: "DetailViewController 詳細画面UI要素があるか") { (activity) in
+            let titleLabel = app.staticTexts["TitleLabel"]
+            let languageLabel = app.staticTexts["LanguageLabel"]
+            let starsLabel = app.staticTexts["StarsLabel"]
+            let watchersLabel = app.staticTexts["WatchersLabel"]
+            let forksLabel = app.staticTexts["ForksLabel"]
+            let openIssuesLabel = app.staticTexts["OpenIssuesLabel"]
+            // 詳細画面が表示されるまで待機
+            waitToAppear(for: titleLabel)
+            XCTAssertTrue(titleLabel.exists, "Detail view title should be displayed")
+            XCTAssertTrue(languageLabel.exists, "Detail view language should be displayed")
+            XCTAssertTrue(starsLabel.exists, "Detail view stars should be displayed")
+            XCTAssertTrue(watchersLabel.exists, "Detail view watchers should be displayed")
+            XCTAssertTrue(forksLabel.exists, "Detail forks language should be displayed")
+            XCTAssertTrue(openIssuesLabel.exists, "Detail open issues should be displayed")
+        }
         
-        // MARK: - 2. 検索結果の一つをタップして DetailViewController に遷移
-        firstCell.tap()
+        XCTContext.runActivity(named: "DetailViewController 戻るボタンを押して SearchViewController に戻る") { (activity) in
+            let backButton = app.navigationBars.buttons.element(boundBy: 0)
+            XCTAssertTrue(backButton.exists, "Back button should exist")
+            backButton.tap()
+        }
         
-        
-        let titleLabel = app.staticTexts["TitleLabel"]
-        let languageLabel = app.staticTexts["LanguageLabel"]
-        let starsLabel = app.staticTexts["StarsLabel"]
-        let watchersLabel = app.staticTexts["WatchersLabel"]
-        let forksLabel = app.staticTexts["ForksLabel"]
-        let openIssuesLabel = app.staticTexts["OpenIssuesLabel"]
-        // 詳細画面が表示されるまで待機
-        waitToAppear(for: titleLabel)
-        XCTAssertTrue(titleLabel.exists, "Detail view title should be displayed")
-        XCTAssertTrue(languageLabel.exists, "Detail view language should be displayed")
-        XCTAssertTrue(starsLabel.exists, "Detail view stars should be displayed")
-        XCTAssertTrue(watchersLabel.exists, "Detail view watchers should be displayed")
-        XCTAssertTrue(forksLabel.exists, "Detail forks language should be displayed")
-        XCTAssertTrue(openIssuesLabel.exists, "Detail open issues should be displayed")
-        
-        // MARK: - 3. 戻るボタンを押して SearchViewController に戻る
-        let backButton = app.navigationBars.buttons.element(boundBy: 0)
-        XCTAssertTrue(backButton.exists, "Back button should exist")
-        backButton.tap()
-        
-        // MARK: - 4. 検索結果が保持されていることを確認
-        // 検索画面が表示されるまで待機
-        waitToAppear(for: firstCell)
-        XCTAssertTrue(firstCell.exists, "Search results should still be displayed after returning")
+        XCTContext.runActivity(named: "DetailViewController 検索結果が保持されていることを確認") { (activity) in
+            // MARK: - 4.
+            // 検索画面が表示されるまで待機
+            let firstCell = app.tables.cells.element(boundBy: 0)
+            waitToAppear(for: firstCell)
+            XCTAssertTrue(firstCell.exists, "Search results should still be displayed after returning")
+        }
     }
 }
