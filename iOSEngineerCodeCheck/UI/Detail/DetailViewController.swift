3fbeb755d1e44556438ba54191ba2ffbc8d756ab
Kent Strong <chihayayome@gmail.com>
2025-02-27 09:04:11 +0900

    Style: Add image and update fonts for stars, watchers, forks, and openIssues

--- iOSEngineerCodeCheck/UI/Detail/DetailViewController.swift
+++ iOSEngineerCodeCheck/UI/Detail/DetailViewController.swift
@@ -28,7 +28,15 @@ final class DetailViewController: UIViewController {
     @IBOutlet private var watchersLabel: UILabel!
     @IBOutlet private var forksLabel: UILabel!
     @IBOutlet private var openIssuesLabel: UILabel!
-
+    @IBOutlet private var starsCountLabel: UILabel!
+    @IBOutlet private var watchersCountLabel: UILabel!
+    @IBOutlet private var forksCountLabel: UILabel!
+    @IBOutlet private var openIssuesCountLabel: UILabel!
+    @IBOutlet private var starsImageView: UIImageView!
+    @IBOutlet private var watchersImageView: UIImageView!
+    @IBOutlet private var forksImageView: UIImageView!
+    @IBOutlet private var openIssuesImageView: UIImageView!
+    
     var viewModel: DetailViewModelProtocol?
     private var cancellables = Set<AnyCancellable>()
 
@@ -73,6 +81,18 @@ final class DetailViewController: UIViewController {
         watchersLabel.font = .preferredFont(forTextStyle: .subheadline)
         forksLabel.font = .preferredFont(forTextStyle: .subheadline)
         openIssuesLabel.font = .preferredFont(forTextStyle: .subheadline)
+        starsCountLabel.textColor = R.color.textColor()
+        watchersCountLabel.textColor = R.color.textColor()
+        forksCountLabel.textColor = R.color.textColor()
+        openIssuesCountLabel.textColor = R.color.textColor()
+        starsImageView.image = UIImage(systemName: "star")
+        watchersImageView.image = UIImage(systemName: "eye")
+        forksImageView.image = UIImage(systemName: "tuningfork")
+        openIssuesImageView.image = UIImage(systemName: "book.circle")
+        starsImageView.tintColor = R.color.textColor()
+        watchersImageView.tintColor = R.color.textColor()
+        forksImageView.tintColor = R.color.textColor()
+        openIssuesImageView.tintColor = R.color.textColor()
         prepareAnimation()
     }
 
@@ -89,16 +109,51 @@ final class DetailViewController: UIViewController {
     private func updateUI(with repository: GithubRepositoryModel) {
         titleLabel.text = repository.fullName
         languageLabel.text = "Written in \(repository.language)"
-        starsLabel.text = "\(repository.stargazersCount) stars"
-        watchersLabel.text = "\(repository.watchersCount) watchers"
-        forksLabel.text = "\(repository.forksCount) forks"
-        openIssuesLabel.text = "\(repository.openIssuesCount) open issues"
+        starsCountLabel.text = "\(repository.stargazersCount)"
+        watchersCountLabel.text = "\(repository.watchersCount)"
+        forksCountLabel.text = "\(repository.forksCount)"
+        openIssuesCountLabel.text = "\(repository.openIssuesCount)"
+        updateCountFonts(with: repository)
         guard let imageURL = URL(string: repository.avatarURL) else {
             imageView.image = UIImage(named: "image-break")
             return
         }
         imageView.kf.indicatorType = .activity
         imageView.kf.setImage(with: imageURL)
+    }
+    
+    private func updateCountFonts(with repository: GithubRepositoryModel) {
+        if repository.stargazersCount > 100 {
+            starsCountLabel.font = .systemFont(ofSize: 16, weight: .medium)
+        } else if repository.stargazersCount > 1000 {
+            starsCountLabel.font = .systemFont(ofSize: 17, weight: .heavy)
+        } else if repository.stargazersCount > 10000 {
+            starsCountLabel.font = .systemFont(ofSize: 18, weight: .bold)
+        } else {
+            starsCountLabel.font = .systemFont(ofSize: 14, weight: .light)
+        }
+        
+        if repository.watchersCount > 100 {
+            watchersCountLabel.font = .systemFont(ofSize: 16, weight: .medium)
+        } else if repository.watchersCount > 1000 {
+            watchersCountLabel.font = .systemFont(ofSize: 17, weight: .heavy)
+        } else if repository.watchersCount > 10000 {
+            watchersCountLabel.font = .systemFont(ofSize: 18, weight: .bold)
+        } else {
+            watchersCountLabel.font = .systemFont(ofSize: 14, weight: .light)
+        }
+        
+        if repository.forksCount > 100 {
+            forksCountLabel.font = .systemFont(ofSize: 16, weight: .medium)
+        } else if repository.forksCount > 1000 {
+            forksCountLabel.font = .systemFont(ofSize: 17, weight: .heavy)
+        } else if repository.forksCount > 10000 {
+            forksCountLabel.font = .systemFont(ofSize: 18, weight: .bold)
+        } else {
+            forksCountLabel.font = .systemFont(ofSize: 14, weight: .light)
+        }
+        
+        openIssuesCountLabel.font = .systemFont(ofSize: 14, weight: .light)
     }
     
     private func prepareAnimation() {
